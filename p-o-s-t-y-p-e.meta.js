cf2a74a1ab9b937a870d40a2ff1c6b6150d2b82a
extrivarnsor <145039587+extrivarnsor@users.noreply.github.com>
2023-09-14 15:25:27 +0000

    up

---
+++ p-o-s-t-y-p-e.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         p-o-s-t-y-p-e
+// @namespace    https://github.com/extrivarnsor/la_loi_chung/raw/main/p-o-s-t-y-p-e.user.js
+// @version      2023.09.14
+// @description  down p-o-s-t-y-p-e
+// @author       You
+// @match        https://original.postype.com/post/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=postype.com
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant        none
+// @downloadURL  https://github.com/extrivarnsor/la_loi_chung/raw/main/p-o-s-t-y-p-e.user.js
+// @updateURL    https://github.com/extrivarnsor/la_loi_chung/raw/main/p-o-s-t-y-p-e.meta.js
+// ==/UserScript==
