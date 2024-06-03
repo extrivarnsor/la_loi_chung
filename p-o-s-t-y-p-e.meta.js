8be97fab1490d9aded5cd4b01b1b902cb30e5c7f
extrivarnsor <145039587+extrivarnsor@users.noreply.github.com>
2024-06-03 23:21:57 +0700

    up

--- p-o-s-t-y-p-e.meta.js
+++ p-o-s-t-y-p-e.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         p-o-s-t-y-p-e
 // @namespace    https://github.com/extrivarnsor/la_loi_chung/raw/main/p-o-s-t-y-p-e.user.js
-// @version      2023.09.14
+// @version      2024.06.03
 // @description  down p-o-s-t-y-p-e
 // @author       You
-// @match        https://original.postype.com/post/*
+// @match        https://www.postype.com/@original/post/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=postype.com
 // @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
 // @grant        none
